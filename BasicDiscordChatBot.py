8a957bc951c3434bd614eb93a4ae1cb4a37a2586
RageStorm76 <96666411+RageStorm76@users.noreply.github.com>
2021-12-25 22:02:32 +0000

    Add files via upload
    Basic discord chat bot, responds to hello, goodbye and can give you a random number

---
+++ BasicDiscordChatBot.py
@@ -0,0 +1,59 @@
+
+import discord
+from discord.ext import commands
+import random
+
+TOKEN = 'OTE3MDM0MjUwODQ5NjM2NDAz.Yay0sw.vpoPcFd-s0_ChX1RNk3HVgNK4wE' # insert your discord token here
+prefix = "+"
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+client = discord.Client()
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    guild = discord.utils.find(lambda g: g.id == payload.guild_id, bot.guilds)
+
+
+
+
+
+# notifies you when bot starts
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == 'crypto-investor-bottest':
+        if user_message.lower() == 'hello':
+            await message.channel.send(f'Hello {username}!')
+            return
+        elif user_message.lower() == 'bye':
+            await message.channel.send(f'See you later {username}!')
+            return
+        elif user_message.lower() == '!random':
+            response = f'This is you random number: {random.randrange(100000)}'
+            await message.channel.send(response)
+            return
+
+
+    if user_message.lower() == '!anywhere':
+        await message.channel.send('This can be used anywhere!')
+        return
+
+
+
+
+
+client.run(TOKEN)
